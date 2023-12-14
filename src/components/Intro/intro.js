9035b360a5ecf15bb763f52b4adc0503be15654e
Ntuthuko Ndlovu <ntuthuko.s.ndlovu@gmail.com>
2023-12-14 15:52:53 +0200

    added the intro file and css/imgs

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import './intro.css'
+import bg from '../../assets/Ntuthuko.jpg';
 
 const Intro = () => {
   return (
-    <div>I</div>
+    <section id='intro'>
+        <div className='introContent'>
+            <span className='hello'>Hello,</span>
+            <span className='introText'>I'm <span className='introName'>Ntu Ndlovu</span>Front-End Developer</span>
+            <p className='introPara'>I am a skilled front-end developer with experience in creating visually appealing and user friendly websites.</p>
+        </div>
+        <img src={bg} alt='Profile' className='bg' />
+    </section>
   )
 }
 
