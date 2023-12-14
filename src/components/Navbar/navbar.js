3a037ba35fe2741b82ad8f733b835386a5ad3de7
Ntuthuko Ndlovu <ntuthuko.s.ndlovu@gmail.com>
2023-12-14 14:03:12 +0200

    push commit

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './navbar.css'
 import logo from "../../assets/logo2.jpg";
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
         <img src={logo}  alt='Logo' className='logo'/>
         <div className='desktopMenu'>
-
+          <link></link>
         </div>
         <button className='desktopMenu'>
             <img src="" alt='logo' className='logo' />
