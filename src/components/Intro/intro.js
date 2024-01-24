06e4ec5c43f402d31d7ed9492f9dcb179259d703
Ntuthuko Ndlovu <ntuthuko.s.ndlovu@gmail.com>
2024-01-24 14:58:33 +0200

    intro fontSize, Content and images.

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import './intro.css'
-import bg from '../../assets/Ntuthuko.jpg';
+import React from 'react';
+import './intro.css';
+import btnImg from '../../assets/Hire call.png';
+import bg from '../../assets/Pro Pic 1.png';
 import { Link } from 'react-scroll';
 
 const Intro = () => {
@@ -10,7 +11,7 @@ const Intro = () => {
             <span className='hello'>Hello,</span>
             <span className='introText'>I'm <span className='introName'>Ntu Ndlovu</span>Front-End Developer</span>
             <p className='introPara'>I am a skilled front-end developer with experience in creating visually appealing and user friendly websites.</p>
-            <Link><button className='btn'><img src='' alt='' /></button></Link>
+            <Link><button className='btn'><img src={btnImg} alt='Hire me' />Hire Me</button></Link>
         </div>
         <img src={bg} alt='Profile' className='bg' />
     </section>
